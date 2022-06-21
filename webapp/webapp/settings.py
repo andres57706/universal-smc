ebdc0f2c28afdbc2d04590d9e96bf9ff9fe9f60a
Andres F Giraldo <andres57706@gmail.com>
2022-06-20 19:59:45 -0500

    feat: permite buscar templates comunes en carpeta templates
    Signed-off-by: Andres F Giraldo <andres57706@gmail.com>

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'webapp.urls'
 
+TEMPLATES_DIR = Path(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
