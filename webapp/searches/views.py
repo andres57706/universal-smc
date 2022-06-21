3a066f996193703d604974068b56a450d0794e2b
Andres F Giraldo <andres57706@gmail.com>
2022-06-20 21:06:49 -0500

    feat: muestra lista de consultas realizadas en index
    Signed-off-by: Andres F Giraldo <andres57706@gmail.com>

--- webapp/searches/views.py
+++ webapp/searches/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from searches.models import Search
+
 
 def index(request):
-    return render(request, 'index/show.html')
+    s_list = Search.objects.all()
+    return render(request, 'index/show.html', context={'items': s_list})
+
 
 def search(request):
-    return render(request, 'results/show.html', context={'status': 200, 'message': f'your message: {request.POST["query-item"]}'})
+    return render(request, 'results/show.html',
+                  context={'status': 200, 'message': f'your message: {request.POST["query-item"]}'})
